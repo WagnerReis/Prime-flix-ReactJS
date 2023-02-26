100701f53cfcc916061df79b80f5bf07282a1419
WagnerReis <wagnerreis1997@gmail.com>
2023-02-26 09:56:12 -0300

    Criando projeto

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Filme from "./pages/Filme";
+import Home from "./pages/Home";
+
+export default function RoutesApp() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home/>} />
+        <Route path="/filme/:id" element={ <Filme/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
